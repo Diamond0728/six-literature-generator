88d2e01b06037100b2146c7a7d9ce78e4ab625dd
Diamond0728 <1015678620@qq.com>
2018-12-26 16:11:19 +0800

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+本项目只是一个文本生成器，莫瞎jb乱用
+
+### 用法
+1. 安装python2
+2. cmd 进入该目录 
+4. pip install -r requirements.txt
+3. python main.py
--- main.py
+++ main.py
@@ -1,9 +1,63 @@
-from flask import Flask
-app = Flask(__name__)
+#-*-coding:utf-8-*-
+import csv
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def write(words):
+    table = open('./data/table.csv','a')
+    write = csv.writer(table)
+    write.writerow(words)
+    table.close()
 
-if __name__ == '__main__':
-    app.run()
+def searchInTable(keyword):
+    print "--- searching in table ---"
+    table = open('./data/table.csv','r')
+    reader = csv.reader(table)
+    for line in reader:
+        if reader.line_num == 1:
+            continue
+        if line[0] == keyword:
+            return line[1]
+    table.close()
+    return False
+
+def searchInBook(keyword):
+    book = './data/JourneyToTheWest.txt'
+    bookObj = open(book,'r')
+    print "--- searching in book ---"
+    for line in bookObj:
+        if keyword in line:
+            return line
+
+
+def addText(keyword, sentence):
+    return '说到 "'+keyword+'" ，我就想起 "'+sentence+'" ，明年年初，中美合拍的西游记即将正式开机，我扮演美猴王孙悟空，我会用美猴王艺术形象努力创造一个正能量的形象，文体两开花，希望大家多多关注。'
+
+
+        
+
+
+def generator(keyword):
+    print keyword
+
+    try:
+        sentence= searchInTable(keyword)
+        if sentence:
+            return addText(keyword,sentence)
+        paragraph = searchInBook(keyword)
+        sentences = paragraph.split("。")
+        for sentence in sentences:
+            if(keyword in sentence):
+                write([keyword, sentence])
+                return addText(keyword,sentence)
+        return addText(keyword,"西游记")
+    except e: 
+        print e
+
+while True:
+    try:
+        keyword = raw_input("请输入关键词:")
+        print generator(keyword)
+    except e: 
+        print "出了一点小问题， 换个词试试看吧！"
